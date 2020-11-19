b57d5b12a7c373425f3e6fe44bc3369d03516178
jeyofdev <jgregoire.pro@gmail.com>
2020-11-19 13:55:25 +0100

    create the Home, History and Personnage Components

---
+++ src/component/History.jsx
@@ -0,0 +1,7 @@
+import react from 'react';
+
+const History = () => {
+  return <div>Home</div>;
+};
+
+export default History;
---
+++ src/component/Personnage.jsx
@@ -0,0 +1,7 @@
+import react from 'react';
+
+const Personnage = () => {
+  return <div>Home</div>;
+};
+
+export default Personnage;
